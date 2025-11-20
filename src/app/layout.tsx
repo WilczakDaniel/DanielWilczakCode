d9cf2fdb8c63c5ba5f13122aed2215e600c32d6d
Daniel Wilczak <danielmarekwilczak@gmail.com>
2025-11-20 18:18:19 +0100

    Zmiana deaful theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,7 +115,7 @@ export default function RootLayout({
       >
         <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
